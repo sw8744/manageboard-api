205f5aa68e811b411c14ed142fa25acc846d856e
thislife_won <sw8744@naver.com>
2023-08-15 23:32:49 +0900

    Fix Bugs

--- server.js
+++ server.js
@@ -5,27 +5,32 @@ const app = express();
 
 app.use(cors());
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
     host: '210.114.22.146',
     user: 'root',
     password: 'ishs123!',
     database: 'test'
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Successfully Connected');
-    }
+pool.getConnection((err, connection) => {
+    connection.connect((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Successfully Connected');
+        }
+    });
 });
+
 
 app.get('/mywriting', (req, res) => {
     const q = req.query;
     const author = q.author;
     if(author != undefined) {
-        connection.query('SELECT * FROM posts WHERE author=' + author, (err, results) => {
-            res.json(results);
+        pool.getConnection((err, connection) => {
+            connection.query('SELECT * FROM posts WHERE author=' + author, (err, results) => {
+                res.json(results);
+            });
         });
     }
     else {
@@ -34,8 +39,10 @@ app.get('/mywriting', (req, res) => {
 });
 
 app.get('/get', (req, res) => {
-    connection.query('SELECT * FROM posts', (err, results) => {
-        res.json(results);
+    pool.getConnection((err, connection) => {
+        connection.query('SELECT * FROM posts', (err, results) => {
+            res.json(results);
+        });
     });
 });
 
@@ -45,9 +52,11 @@ app.get('/post', (req, res) => {
     const author = q.author;
     const content = q.content;
     if(title != undefined && author != undefined && content != undefined) {
-        connection.query(`INSERT INTO posts (title, author, content) VALUES ('${title}', ${author}, '${content}')`, (err, results) => {
-            if (err) throw err;
-            res.json(results);
+        pool.getConnection((err, connection) => {
+            connection.query(`INSERT INTO posts (title, author, content) VALUES ('${title}', ${author}, '${content}')`, (err, results) => {
+                if (err) throw err;
+                res.json(results);
+            });
         });
     }
     else {
