502af01701014ad4b9cec1c41e66cbf9e4bd954e
thislife_won <sw8744@naver.com>
2023-08-15 22:52:02 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: '210.114.22.146',
+    user: 'root',
+    password: 'ishs123!',
+    database: 'test'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully Connected');
+    }
+});
+
+app.get('/mywriting', (req, res) => {
+    const q = req.query;
+    const author = q.author;
+    if(author != undefined) {
+        connection.query('SELECT * FROM posts WHERE author=' + author, (err, results) => {
+            res.json(results);
+        });
+    }
+    else {
+        res.json('Error');
+    }
+});
+
+app.get('/post', (req, res) => {
+    const q = req.query;
+    const title = q.title;
+    const author = q.author;
+    const content = q.content;
+    if(title != undefined && author != undefined && content != undefined) {
+        connection.query(`INSERT INTO posts (title, author, content) VALUES ('${title}', ${author}, '${content}')`, (err, results) => {
+            if (err) throw err;
+            res.json(results);
+        });
+    }
+    else {
+        res.json('Error');
+    }
+});
+
+app.listen(3000);
